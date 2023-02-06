2a0de54c44fd356fea166dc95a09935e9e77bfc1
vishal-vadodariya <vadodariyavishal2@gmail.com>
2023-02-06 18:48:11 +0530

    update

--- session html/script.js
+++ session html/script.js
@@ -2,8 +2,13 @@ function initiate()
 {
     var saveButton = document.getElementById('save');
     var retrieveButton = document.getElementById('retrieve');
+    var deleteButton = document.getElementById('delete');
+    var reviewButton = document.getElementById('review');
+
     saveButton.addEventListener('click', saveItem);
     retrieveButton.addEventListener('click', retrieveItem);
+    deleteButton.addEventListener('click', deleteItem);
+    reviewButton.addEventListener('click', reviewAll);
 }
 
 function saveItem() 
@@ -20,6 +25,26 @@ function retrieveItem(){
     var key = document.getElementById('key').value;
     var value = sessionStorage[key];
     data.innerHTML = '<div>' + key + ': ' + value + '<div>'; 
+}
+
+function deleteItem(){
+    if(confirm('Delete?'))
+    {
+        var key = document.getElementById('key').value;
+        sessionStorage.removeItem(key);
+        data.innerHTML = '<div>Deleted.</div>';
+    }
+}
+
+function reviewAll()
+{
+    for (var i = 0 ; i < sessionStorage.length ; i++) 
+    {
+        var key = sessionStorage.key(i);
+        var value = sessionStorage(key);
+        data.innerHTML += '<div>' + key + ': ' + value + '<br><div>'; 
+        
+    }
 }
 
 addEventListener("load", initiate);
